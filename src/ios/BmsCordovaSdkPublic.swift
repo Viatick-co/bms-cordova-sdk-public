a8778c62dc9b7b284137fbba6e9cdf6ba3bba124
Bie Yaqing <yaqing.bie@users-MacBook-Pro.local>
2018-05-03 18:04:26 +0800

    update

--- src/ios/BmsCordovaSdkPublic.swift
+++ src/ios/BmsCordovaSdkPublic.swift
@@ -1,4 +1,18 @@
 @objc(BmsCordovaSdkPublic) class BmsCordovaSdkPublic : CDVPlugin {
+
+	let viaBmsCtrl = ViaBmsCtrl.sharedInstance;
+
+	@objc(initSDK:)
+	func initSDK(command: CDVInvokedUrlCommand) {
+		var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR);
+
+		let sdk_key = command.arguments[0] as? String ?? "no value";
+		viaBmsCtrl.initSdk(uiViewController: self.viewController, sdk_key: sdk_key);
+
+		pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "initSDK done!");
+		self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+	}
+
 	@objc(initCustomer:)
 	func initCustomer(command: CDVInvokedUrlCommand) {
 		var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR);
@@ -6,11 +20,46 @@
 		let identifier = command.arguments[0] as? String ?? "no value";
 		let phone = command.arguments[1] as? String ?? "no value";
 		let email = command.arguments[2] as? String ?? "no value";
+		viaBmsCtrl.initCustomer(identifier: identifier, phone: phone, email: email);
 
-		print("BmsCordovaSdkPublic", identifier, phone, email);
-
-		pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "done");
+		pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "initCustomer done!");
 
 		self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
 	}
+
+	@objc(setting:)
+    func setting(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR);
+        
+        let alert = command.arguments[0] as? Bool ?? false;
+        let background = command.arguments[1] as? Bool ?? false;
+        let site = command.arguments[2] as? Bool ?? false;
+        let attendance = command.arguments[3] as? Bool ?? false;
+        let tracking = command.arguments[5] as? Bool ?? false;
+        viaBmsCtrl.setting(alert: alert, background: background, site: site, attendance: attendance, tracking: tracking);
+        
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "setting done!");
+        
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
+
+    @objc(startSDK:)
+    func startSDK(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR);
+        
+        viaBmsCtrl.startBmsService();
+        
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "startSDK done!");
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
+    
+    @objc(endSDK:)
+    func endSDK(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR);
+        
+        viaBmsCtrl.stopBmsService();
+        
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "endSDK done!");
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
 }
--- www/BmsCordovaSdkPublic.js
+++ www/BmsCordovaSdkPublic.js
@@ -1,5 +1,21 @@
 var exec = require('cordova/exec');
 
+exports.initSDK = function (sdkKey, success, error) {
+    exec(success, error, 'BmsCordovaSdkPublic', 'initSDK', [sdkKey]);
+};
+
 exports.initCustomer = function (identifier, phone, email, success, error) {
     exec(success, error, 'BmsCordovaSdkPublic', 'initCustomer', [identifier, phone, email]);
+};
+
+exports.setting = function (alert, background, site, attendance, tracking, success, error) {
+    exec(success, error, 'BmsCordovaSdkPublic', 'setting', [alert, background, site, attendance, tracking]);
+};
+
+exports.startSDK = function (success, error) {
+    exec(success, error, 'BmsCordovaSdkPublic', 'startSDK', []);
+};
+
+exports.stopSDK = function (success, error) {
+    exec(success, error, 'BmsCordovaSdkPublic', 'stopSDK', []);
 };
